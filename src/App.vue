d378f83de7ce5bf9c8b39a5fc42c8474acc69ad9
michal-cic <michal.cic@gmail.com>
2022-02-09 17:06:45 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <TabGroup>
+    <TabList>
+      <Tab>Tab 1</Tab>
+      <Tab>Tab 2</Tab>
+      <Tab>Tab 3</Tab>
+    </TabList>
+    <TabPanels>
+      <TabPanel>Content 1</TabPanel>
+      <TabPanel>
+        <HelloWorld />
+      </TabPanel>
+      <TabPanel>Content 3</TabPanel>
+    </TabPanels>
+  </TabGroup>
+</template>
+
+<script>
+  import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    components: {
+      TabGroup,
+      TabList,
+      Tab,
+      TabPanels,
+      TabPanel,
+      HelloWorld
+    },
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <!--
+    Pass the `isOpen` ref to the `open` prop, and use the `close` event
+    to set the ref back to `false` when the user clicks outside of
+    the dialog or presses the escape key.
+  -->
+  <Dialog :open="isOpen" @close="setIsOpen">
+    <DialogOverlay />
+
+    <DialogTitle>Deactivate account</DialogTitle>
+    <DialogDescription>
+      This will permanently deactivate your account
+    </DialogDescription>
+
+    <p>
+      Are you sure you want to deactivate your account? All of your data will be
+      permanently removed. This action cannot be undone.
+    </p>
+
+    <!--
+      You can render additional buttons to dismiss your dialog by setting your
+      `isOpen` state to `false`. 
+    -->
+    <button @click="setIsOpen(false)">Cancel</button>
+    <button @click="handleDeactivate">Deactivate</button>
+  </Dialog>
+</template>
+
+<script>
+  import { ref } from "vue";
+  import {
+    Dialog,
+    DialogOverlay,
+    DialogTitle,
+    DialogDescription,
+  } from "@headlessui/vue";
+
+  export default {
+    components: { Dialog, DialogOverlay, DialogTitle, DialogDescription },
+    setup() {
+      // The open/closed state lives outside of the Dialog and
+      // is managed by you.
+      let isOpen = ref(true);
+
+      return {
+        isOpen,
+        setIsOpen(value) {
+          isOpen.value = value;
+        },
+        handleDeactivate() {
+          // ...
+        }
+      };
+    },
+  };
+</script>
